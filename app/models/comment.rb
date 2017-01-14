cda45f0cc3889fb95d0b01821f06f9d8faf98c8a
nitamon314 <nitamon314@gmail.com>
2017-01-14 10:20:44 +0000

    直し2

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :customer
-  has_many :users
+  belongs_to :user
   
   validates :body,          presence: true, length: {maximum: 200}
   validates :customer_id,   presence: true
